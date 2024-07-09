2ee562b1d535735827ede96e1a0d76105f36f3aa
Swapnil Patil <patil.k.swapnil@gmail.com>
2024-07-09 12:23:32 -0230

    Adds solutions for 15 problems

---
+++ asteroid_collision.rb
@@ -0,0 +1,54 @@
+=begin
+We are given an array asteroids of integers representing asteroids in a row.
+
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+Example 1:
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+
+Example 2:
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+
+Example 3:
+
+Input: asteroids = [10,2,-5]
+Output: [10]
+Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+
+Constraints:
+
+2 <= asteroids.length <= 104
+-1000 <= asteroids[i] <= 1000
+asteroids[i] != 0
+
+=end
+
+# @param {Integer[]} asteroids
+# @return {Integer[]}
+def asteroid_collision(asteroids)
+  stack = []
+
+  asteroids.each do |asteroid|
+    while !stack.empty? && asteroid < 0 && stack.last > 0
+      diff = asteroid + stack.last
+      if diff < 0
+        stack.pop
+      elsif diff > 0
+        asteroid = 0
+      else
+        asteroid = 0
+        stack.pop
+      end
+    end
+    stack.push(asteroid) if asteroid != 0
+  end
+  stack
+end
---
+++ decode_string.rb
@@ -0,0 +1,54 @@
+=begin
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output will never exceed 105.
+
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+
+Constraints:
+
+1 <= s.length <= 30
+s consists of lowercase English letters, digits, and square brackets '[]'.
+s is guaranteed to be a valid input.
+All the integers in s are in the range [1, 300].
+=end
+
+# @param {String} s
+# @return {String}
+def decode_string(s)
+  stack = []
+  s.each_char do |c|
+    if c == ']'
+      s1 = ''
+      while stack.last != '['
+        s1 = stack.pop + s1
+      end
+      stack.pop
+      n = ''
+      while !stack.empty? && stack.last.match?(/[0-9]/)
+        n = stack.pop + n
+      end
+      stack.push(s1 * n.to_i)
+    else
+      stack.push(c)
+    end
+  end
+  stack.join
+end
